7160fea5ec08cdeb4bffbf2504f69f6ad46a6a2b
Noah <nbelman1@gmail.com>
2025-07-07 21:56:49 -0400

    Added password requirements, do-while loop, and if-else statement.

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-code
+const input = require('readline-sync');
+
+let metReqs = false;
+let counter = 0;
+
+do {
+    counter++;
+    let password = input.question("Enter your password: ");
+    if (password.length > 8) { // if password is longer than 8 characters
+        for (char of password) {
+            if (char >= 'A' && char <= 'Z') { // (character comparison) if password contains uppercase letter
+                for (char of password) {
+                    if (char >= '0' && char <= '9') { // (character comparison) if password contains number 
+                        metReqs = true;
+                        break; 
+                    }
+                }
+            } 
+        }
+    } else {
+        break;
+    }
+} while (counter < 1);
+
+if (metReqs) {
+        console.log("Password validation successful!");
+} else {
+    console.log("Your password does not meet the requirements.");
+}
